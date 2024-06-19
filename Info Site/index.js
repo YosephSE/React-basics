3eea890b88f21970ef06851d64dc893b1c7919ad
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-06-20 01:47:12 +0300

    few updates

--- Info Site/index.js
+++ Info Site/index.js
@@ -1,21 +1,8 @@
-/*
-Challenge: Starting from scratch, build and render the 
-HTML for our section project. Check the Google slide for 
-what you're trying to build.
 
-We'll be adding styling to it later.
-
-Hints:
-* The React logo is a file in the project tree, so you can
-  access it by using `src="./react-logo.png" in your image
-  element
-* You can also set the `width` attribute of the image element
-  just like in HTML. In the slide, I have it set to 40px
- */
-
-const main = (
+function ReactApp(){
+  return (
     <div>
-        <img src="logo.png" />
+        <img src="logo.png" width="40px"/>
         <h1>Fun Facts About React!</h1>
         <ul>
             <li>Was first released in 2013</li>
@@ -25,4 +12,5 @@ const main = (
         </ul>
     </div>
 )
-ReactDOM.render(main, document.querySelector('#root'))
+}
+ReactDOM.render(<ReactApp/>, document.querySelector('#root'))

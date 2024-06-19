6696e4da976c7e5ad86f325f3d5cad480128f047
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-06-20 02:25:29 +0300

    css styles added:

--- Info Site/index.js
+++ Info Site/index.js
@@ -12,8 +12,8 @@ function HeaderNav() {
   return (
     <header>
       <nav>
-        <img src="logo.png" />
-        <ul>
+        <ul className="flex">
+          <img src="logo.png" />
           <li>React</li>
           <li>About</li>
         </ul>
@@ -23,21 +23,20 @@ function HeaderNav() {
 }
 function Main() {
   return (
-  <div>
-    <h1>Fun Facts About React!</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100k stars on Github</li>
-      <li>It is maintained by Facebook</li>
-    </ul>
-  </div>)
+    <div>
+      <h1>Fun Facts About React!</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>It is maintained by Facebook</li>
+      </ul>
+    </div>
+  );
 }
 
 function Footer() {
-  return (
-    <footer>© 2024 Yoseph Kedir. All rights reserved.</footer>
-  )
+  return <footer><small>© 2024 Yoseph Kedir. All rights reserved.</small></footer>;
 }
 
 ReactDOM.render(<Page />, document.querySelector("#root"));
